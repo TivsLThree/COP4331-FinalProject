03752e00493e964fdbefaa38f8d7fa7e6d98693a
Levi <levihuchingson@gmail.com>
2019-11-22 15:01:57 -0500

    ViewCardPAge

---
+++ ViewCardPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class ViewCardPage extends React.Component
+{
+
+  loadCards()
+  {
+    return (
+        <div className="card-columns">
+        </div>
+      )
+  }
+  render ()
+  {
+    return (
+        <h1>View your dumb pictures </h1>
+    )
+  }
+}
+
+export default ViewCardPage;
